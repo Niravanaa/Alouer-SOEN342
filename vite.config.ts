126b22af7a2e87e7b4ea4bab3b9c7a812ee7bb9c
Nirav Patel <niravp0703@gmail.com>
2024-10-18 23:44:21 -0400

    fix: add resolve for enum import in client-side

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,10 @@ export default defineConfig({
   test: {
     include: ["src/**/*.test.{js,ts}"],
   },
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser":
+        "./node_modules/.prisma/client/index-browser.js",
+    },
+  },
 });
