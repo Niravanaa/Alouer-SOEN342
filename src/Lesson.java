2c2d057b07e250e066dfe99c2705a4b47b324318
Laurenz Gomez <golimaarrr@gmail.com>
2024-10-22 19:38:15 -0400

    feature: create domain model class files
    Create classes mentioned in the domain model

---
+++ src/Lesson.java
@@ -0,0 +1,9 @@
+public class Lesson {
+
+    public enum LessonType {
+        PRIVATE, SEMI_PRIVATE, GROUP
+    }
+
+    private int id;
+    private LessonType type;
+}
---
+++ src/Location.java
@@ -0,0 +1,10 @@
+public class Location {
+    private int id;
+    private String name;
+    private String address;
+    private String city;
+    private String province;
+    private int instructorId; // int?
+
+    private Lesson[] lesson;
+}
